f37be57237659741e8f10d0adce9fc27ab4ec839
ozcanmelih <melih.ozcan89@gmail.com>
2019-04-11 19:21:56 +0300

    EvenLog getter setter

--- 404-be/src/main/java/com/team4/jpa/entity/EventLog.java
+++ 404-be/src/main/java/com/team4/jpa/entity/EventLog.java
@@ -18,4 +18,44 @@ public class EventLog extends  EntityBase {
 
     public EventLog() {
     }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+
+    public Integer getDecision() {
+        return decision;
+    }
+
+    public void setDecision(Integer decision) {
+        this.decision = decision;
+    }
+
+    public String getComparisonStr() {
+        return comparisonStr;
+    }
+
+    public void setComparisonStr(String comparisonStr) {
+        this.comparisonStr = comparisonStr;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public Candidate getCandidate() {
+        return candidate;
+    }
+
+    public void setCandidate(Candidate candidate) {
+        this.candidate = candidate;
+    }
 }
